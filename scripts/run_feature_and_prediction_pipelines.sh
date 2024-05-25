1be781d806c32346794decb43548426cef780999
tobiasmj97 <tobias.mk97@gmail.com>
2024-05-25 13:45:24 +0200

    actions

--- scripts/run_feature_and_prediction_pipelines.sh
+++ scripts/run_feature_and_prediction_pipelines.sh
@@ -7,5 +7,8 @@ cd notebooks
 # Run the feature pipeline
 jupyter nbconvert --to notebook --execute 2_feature_pipeline.ipynb
 
-# Run the batch inference pipeline
-jupyter nbconvert --to notebook --execute 4_batch_inference.ipynb
+# Run the batch inference pipeline (xgboost)
+jupyter nbconvert --to notebook --execute 4_1_inference_pipeline_xgb.ipynb
+
+# Run the batch inference pipeline (lstm)
+jupyter nbconvert --to notebook --execute 4_2_inference_pipeline_lstm.ipynb
